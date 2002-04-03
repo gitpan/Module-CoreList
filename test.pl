c17364ba0c585892e165203805498052667aef5f
Richard Clamp <richardc@unixbeard.net>
2002-04-03 10:49:38 +0000

    Import of RCLAMP/Module-CoreList-1.4 from CPAN.
    gitpan-cpan-distribution: Module-CoreList
    gitpan-cpan-version:      1.4
    gitpan-cpan-path:         RCLAMP/Module-CoreList-1.4.tar.gz
    gitpan-cpan-author:       RCLAMP
    gitpan-cpan-maturity:     released

--- test.pl
+++ test.pl
@@ -8,18 +8,18 @@ ok($Module::CoreList::version{5.00503},    "5.00503");
 ok(!exists $Module::CoreList::version{5.00503}{attributes},
    "attributes weren't in 5.00503");
 
-ok($Module::CoreList::version{5.00601},    "5.00601");
+ok($Module::CoreList::version{5.006001},    "5.006001");
 
-ok(exists $Module::CoreList::version{'5.00601'}{attributes},
+ok(exists $Module::CoreList::version{'5.006001'}{attributes},
    "attributes were in 5.6.1");
 
-ok($Module::CoreList::version{5.00703},    "5.00703");
+ok($Module::CoreList::version{5.007003},    "5.007003");
 
-ok(exists $Module::CoreList::version{5.00703}{'Attribute::Handlers'},
+ok(exists $Module::CoreList::version{5.007003}{'Attribute::Handlers'},
    "Attribute::Handlers were bundled with 5.7.3");
 
 is(Module::CoreList->first_release('File::Spec'), 5.005_03,
    "File::Spec was first bundled in 5.005_03");
 
-is(Module::CoreList->first_release('File::Spec', 0.82), 5.006_01,
-   "File::Spec reached 0.82 with 5.006_01");
+is(Module::CoreList->first_release('File::Spec', 0.82), 5.006_001,
+   "File::Spec reached 0.82 with 5.006_001");
